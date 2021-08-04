a0aa0926aa9f59f89d04b9e6b892c6d7042803a8
goz4el <hordemzerado@gmail.com>
2021-08-04 10:39:40 -0400

    Improving the views, adding complete html to templates and url paths instead of hardcoded ones

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from .models import Question, Choice
+from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = get_list_or_404(Question.objects.order_by('-pub_date')[:5])
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question}) 
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
